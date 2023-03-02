1c24896966d5f4dee694217521be3e209d1a45ee
Corvin Levy <aidancrafter@gmail.com>
2023-03-02 08:07:11 -0800

    Initial commit.

---
+++ src/main/java/org/twotails/borealis/Borealis.java
@@ -0,0 +1,15 @@
+package org.twotails.borealis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Borealis extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
